073778e0c77475394d75b0bdc1236723e7823d3d
christanfox <bayshorestinks@aol.com>
2022-06-02 21:33:43 -0400

    commented out previous code

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,4 +9,23 @@ const resolvers = {
   }
 };
 
+// const resolvers = {
+//   Query: {
+//     services: async (parent, name) => {
+//       return Service.find();
+//     },
+//     service: async (parent, { _id }) => {
+//       return Service.findOne({ _id });
+//     },
+//     users: async () => {
+//       return User.find().select("-__v -password");
+//     },
+//     user: async (parent, { username }) => {
+//       return User.findOne({ username })
+//         .select("-__v -password")
+//         .populate("orders");
+//     },
+//   },
+// };
+
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,4 +6,36 @@ const typeDefs = gql`
   }
 `;
 
+// const typeDefs = gql`
+//   type Service {
+//     _id: ID
+//     name: String
+//     description: String
+//     image: String
+//     quantity: Int
+//     price: Float
+//   }
+//   type Order {
+//     _id: ID
+//     purchaseDate: String
+//     services: [Service]
+//   }
+//   type User {
+//     _id: ID
+//     firstName: String
+//     lastName: String
+//     email: String
+//     orders: [Order]
+//   }
+//   type Auth {
+//     token: ID
+//     user: User
+//   }
+//   type Query {
+//     services(_id: ID!, name: String): [Service]
+//     user: User
+//     order(_id: ID!): Order
+//   }
+// `;
+
 module.exports = typeDefs;
