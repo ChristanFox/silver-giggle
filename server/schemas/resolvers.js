9505ed2d24805e65c0cc66ab0caebe21ea41766e
unknown <avrumiesafranovitz@gmail.com>
2022-06-02 21:59:25 -0400

    added seeds

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,12 @@
 // import the gql tagged template function
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const resolvers = {
   Query: {
     helloWorld: () => {
-      return 'Hello world!';
-    }
-  }
+      return "Hello world!";
+    },
+  },
 };
 
 // const resolvers = {
