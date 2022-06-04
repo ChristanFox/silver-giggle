85c8e0574261adc0e52df358197dc006bb9ff647
christanfox <bayshorestinks@aol.com>
2022-06-04 15:16:35 -0400

    working on signup

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,18 @@
 import React from "react";
+// import { Link } from "react-router-dom";
 
 const Header = () => {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
+        {/* <Link to="/"> */}
         <h1>Oui'd Cavern</h1>
+        {/* </Link> */}
       </div>
     </header>
   );
