c660b6a31df63d11d908ad81e22fbfa782d203e3
pcion <patrick.ciongoli@gmail.com>
2022-06-07 14:00:28 -0400

    updated homepage layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
+   ApolloClient,
+   InMemoryCache,
+   ApolloProvider,
+   createHttpLink,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
@@ -16,42 +16,42 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 const httpLink = createHttpLink({
-  uri: "/graphql",
+   uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("id_token");
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
+   const token = localStorage.getItem("id_token");
+   return {
+      headers: {
+         ...headers,
+         authorization: token ? `Bearer ${token}` : "",
+      },
+   };
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+   link: authLink.concat(httpLink),
+   cache: new InMemoryCache(),
 });
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div className="flex-column justify-flex-start min-100-vh">
-          <Header />
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-            </Routes>
-          </div>
-          <Footer />
-        </div>
-      </Router>
-    </ApolloProvider>
-  );
+   return (
+      <ApolloProvider client={client}>
+         <Router>
+            <div className="flex-column justify-flex-start min-100-vh">
+               <Header />
+               <div className="container">
+                  <Routes>
+                     <Route path="/" element={<Home />} />
+                     <Route path="/login" element={<Login />} />
+                     <Route path="/signup" element={<Signup />} />
+                  </Routes>
+               </div>
+               <Footer />
+            </div>
+         </Router>
+      </ApolloProvider>
+   );
 }
 
 export default App;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,56 +3,75 @@ import React from "react";
 const Home = () => {
    return (
       <main>
-         <div className="flex-row justify-space-between">
-            <div className="col-12 mb-3">Home.js</div>
-         </div>
-         <section className="testimonials">
-            <div className="container">
-               <div className="row">
-                  <div className="col-sm-12">
-                     <div id="customers-testimonials" className="owl-carousel">
-                        <div className="item">
-                           <div className="shadow-effect">
-                              <img
-                                 className="img-circle"
-                                 src="https://assets.codepen.io/7125791/testimonal-img-1_2.jpg"
-                                 alt="img"
-                              />
-                              <p></p>
-                           </div>
-                           <div className="testimonial-name">
-                              
-                           </div>
-                        </div>
-                        <div className="item">
-                           <div className="shadow-effect">
-                              <img
-                                 className="img-circle"
-                                 src="https://assets.codepen.io/7125791/testimonal-img-4_1.jpg"
-                                 alt="img"
-                              />
-                              <p>kj</p>
-                           </div>
-                           <div className="testimonial-name">
-                             <p>
-                             The one and only place I shop for my products</div>
-                        </div>
-                        <div className="item">
-                           <div className="shadow-effect">
-                              <img
-                                 className="img-circle"
-                                 src="https://assets.codepen.io/7125791/testimonal-img-8_1.jpg"
-                                 alt="img"
-                              />
-                              <p></p>
-                           </div>
-                           <div className="testimonial-name">These products cured my anxiety!</div>
-                        </div>
+         <div className="flex-row text-center">
+            <div className="col-12 mb-3">
+               <div className="justify-content-center">
+                  <div class="jumbotron jumbotron-fluid">
+                     <div class="container">
+                        <h1>jumbotron</h1>
+                        <p>
+                           <p>Oui'd Cavern is out of this world!</p>
+                           <br></br>
+                           <p>
+                              Contrary to popular belief, Lorem Ipsum is not
+                              simply random text. It has roots in a piece of
+                              classical Latin literature from 45 BC, making it
+                              over 2000 years old. Richard McClintock, a Latin
+                              professor at Hampden-Sydney College in Virginia,
+                              looked up one of the more obscure Latin words,
+                              consectetur, from a Lorem Ipsum passage, and going
+                              through the cites of the word in classical
+                              literature, discovered the undoubtable source.
+                              Lorem Ipsum comes from sections 1.10.32 and
+                              1.10.33 of "de Finibus Bonorum et
+                           </p>
+                        </p>
                      </div>
                   </div>
                </div>
             </div>
-         </section>
+         </div>
+         <div className="container">
+            <div className="flex-row text-center ">
+               <div className="item col-12 col-md-4">
+                  <div>
+                     <img
+                        className="img-circle"
+                        src="https://assets.codepen.io/7125791/testimonal-img-1_2.jpg"
+                        alt="img"
+                     />
+                  </div>
+                  <div className="testimonial-example">
+                     <p>Oui'd Cavern is out of this world!</p>
+                  </div>
+               </div>
+
+               <div className="item col-12 col-md-4">
+                  <img
+                     className="img-circle"
+                     src="https://assets.codepen.io/7125791/testimonal-img-4_1.jpg"
+                     alt="img"
+                  />
+                  <div className="testimonial-example">
+                     <p>The one and only place I shop for my products</p>
+                  </div>
+               </div>
+
+               <div className="item col-12 col-md-4">
+                  <img
+                     className="img-circle"
+                     src="https://assets.codepen.io/7125791/testimonal-img-8_1.jpg"
+                     alt="img"
+                  />
+                  <div className="testimonial-example">
+                     <p>
+                        I have yet to be dissapointed with this companies
+                        products and customer service, keep up the great work!
+                     </p>
+                  </div>
+               </div>
+            </div>
+         </div>
       </main>
    );
 };
