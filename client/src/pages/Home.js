610706df5d2008405f9a6935e10df6339bd8a928
pcion <patrick.ciongoli@gmail.com>
2022-06-07 19:51:44 -0400

    updated jumbo

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,68 +6,74 @@ const Home = () => {
          <div className="flex-row text-center">
             <div className="col-12 mb-3">
                <div className="justify-content-center">
-                  <div class="jumbotron jumbotron-fluid">
-                     <div class="container">
-                        <h1>jumbotron</h1>
+                  <div className="jumbotron">
+                     <div className="container">
+                        <h1>Oui'd Cavern</h1>
+                        <br></br>
+                        <br></br>
                         <p>
-                           <p>Oui'd Cavern is out of this world!</p>
-                           <br></br>
-                           <p>
-                              Contrary to popular belief, Lorem Ipsum is not
-                              simply random text. It has roots in a piece of
-                              classical Latin literature from 45 BC, making it
-                              over 2000 years old. Richard McClintock, a Latin
-                              professor at Hampden-Sydney College in Virginia,
-                              looked up one of the more obscure Latin words,
-                              consectetur, from a Lorem Ipsum passage, and going
-                              through the cites of the word in classical
-                              literature, discovered the undoubtable source.
-                              Lorem Ipsum comes from sections 1.10.32 and
-                              1.10.33 of "de Finibus Bonorum et
-                           </p>
+                           Welcome to Oui'd where we provide you with all of
+                           your essential CBD needs.
+                        </p>
+                        <br></br>
+                        <p>
+                           Ooi'd essential oil's have always been around you
+                           just didnt have us to get it to you!
                         </p>
                      </div>
                   </div>
                </div>
             </div>
          </div>
-         <div className="container">
-            <div className="flex-row text-center ">
-               <div className="item col-12 col-md-4">
-                  <div>
+
+         <div class="hero-section">
+            <div class="card-home-grid">
+               <div class="card-home">
+                  <div class="card-home__background">
                      <img
-                        className="img-circle"
                         src="https://assets.codepen.io/7125791/testimonal-img-1_2.jpg"
                         alt="img"
                      />
                   </div>
-                  <div className="testimonial-example">
-                     <p>Oui'd Cavern is out of this world!</p>
+                  <div class="card-home__content">
+                     <p class="card-home__category">Ooui'd</p>
+                     <h3 class="card-home__heading">Amazing Products! </h3>
                   </div>
                </div>
-
-               <div className="item col-12 col-md-4">
-                  <img
-                     className="img-circle"
-                     src="https://assets.codepen.io/7125791/testimonal-img-4_1.jpg"
-                     alt="img"
-                  />
-                  <div className="testimonial-example">
-                     <p>The one and only place I shop for my products</p>
+               <div class="card-home">
+                  <div class="card-home__background">
+                     <img
+                        src="https://assets.codepen.io/7125791/testimonal-img-4_1.jpg"
+                        alt="img"
+                     />
+                  </div>
+                  <div class="card-home__content">
+                     <p class="card-home__category">Ooui'd</p>
+                     <h3 class="card-home__heading">Best services 'Period'.</h3>
                   </div>
                </div>
-
-               <div className="item col-12 col-md-4">
-                  <img
-                     className="img-circle"
-                     src="https://assets.codepen.io/7125791/testimonal-img-8_1.jpg"
-                     alt="img"
-                  />
-                  <div className="testimonial-example">
-                     <p>
-                        I have yet to be dissapointed with this companies
-                        products and customer service, keep up the great work!
-                     </p>
+               <div class="card-home">
+                  <div class="card-home__background">
+                     <img
+                        src="https://assets.codepen.io/7125791/testimonal-img-8_1.jpg"
+                        alt="img"
+                     />
+                  </div>
+                  <div class="card-home__content">
+                     <p class="card-home__category">Ooui'd</p>
+                     <h3 class="card-home__heading">Great Oui'd! </h3>
+                  </div>
+               </div>
+               <div class="card-home">
+                  <div class="card-home__background">
+                     <img
+                        src="https://assets.codepen.io/7125791/testimonial-img-6_1.jpg"
+                        alt="img"
+                     />
+                  </div>
+                  <div class="card-home__content">
+                     <p class="card-home__category">Ooui'd</p>
+                     <h3 class="card-home__heading">Everyone's using it! </h3>
                   </div>
                </div>
             </div>
@@ -77,3 +83,46 @@ const Home = () => {
 };
 
 export default Home;
+
+{
+   /* 
+<div className="flex-row text-center ">
+   <div className="item col-12 col-md-4">
+      <div>
+         <img
+            className="img-circle"
+            src="https://assets.codepen.io/7125791/testimonal-img-1_2.jpg"
+            alt="img"
+         />
+      </div>
+      <div className="testimonial-example">
+         <p>Oui'd Cavern is out of this world!</p>
+      </div>
+   </div>
+
+   <div className="item col-12 col-md-4">
+      <img
+         className="img-circle"
+         src="https://assets.codepen.io/7125791/testimonal-img-4_1.jpg"
+         alt="img"
+      />
+      <div className="testimonial-example">
+         <p>The one and only place I shop for my products</p>
+      </div>
+   </div>
+
+   <div className="item col-12 col-md-4">
+      <img
+         className="img-circle"
+         src="https://assets.codepen.io/7125791/testimonial-img-6_1.jpg"
+         alt="img"
+      />
+      <div className="testimonial-example">
+         <p>
+            I have yet to be dissapointed with this companies products and
+            customer service, keep up the great work!
+         </p>
+      </div>
+   </div>
+</div>; */
+}
