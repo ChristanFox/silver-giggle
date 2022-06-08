606fb5da47d756c5a7f2c444689eaa2eb68e83e0
Carolyn B <bmathematics@gmail.com>
2022-06-08 15:01:52 -0400

    made changes to connection to support deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/silver-giggles",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/silver-giggles",
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useUnifiedTopology: true,
   }
 );
 
