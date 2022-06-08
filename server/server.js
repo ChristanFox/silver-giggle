f9864347738c61a3c0828f73558dacd3f395e5f4
christanfox <bayshorestinks@aol.com>
2022-06-08 17:56:03 -0400

    deploy

--- server/server.js
+++ server/server.js
@@ -18,23 +18,23 @@ const server = new ApolloServer({
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   // integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
-
-  app.use(express.urlencoded({ extended: false }));
-  app.use(express.json());
-
-  // Serve up static assets
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
-  }
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
 
   db.once("open", () => {
     app.listen(PORT, () => {
