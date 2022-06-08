d55cc1c282baa41c9573686baf8e93784ed95850
pcion <patrick.ciongoli@gmail.com>
2022-06-08 15:52:29 -0400

    linked Services to Product/Product to Home. updated style

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -2,72 +2,77 @@ import React from "react";
 const Product = () => {
    return (
       <div className="container text ">
-         <div className="header">
+         <div className="jumbotron text-center">
             <h1>CONTACT US &nbsp;</h1>
             <h3>
                Fill the form to let us know what service you would like to
                inquire about!
             </h3>
-         </div>
-         <br></br>
-         <div className=" container">
-            <div className="container">
-               <form id="contact" action="/" className="text-center">
-                  <h4>Reach out to us to schedule your Oui'd appointment!</h4>
-                  <fieldset>
-                     <input
-                        placeholder="Full Name"
-                        type="text"
-                        tabindex="1"
-                        required
-                        autofocus
-                     />
-                  </fieldset>
-                  <fieldset>
-                     <input
-                        placeholder="Email"
-                        type="email"
-                        tabindex="1"
-                        required
-                     />
-                  </fieldset>
-                  <fieldset>
-                     <input
-                        placeholder="Best # to reach you at"
-                        type="tel"
-                        tabindex="3"
-                        required
-                     />
-                  </fieldset>
-                  <fieldset>
-                     <input
-                        placeholder="Date of appointment"
-                        type="date"
-                        tabindex="4"
-                        required
-                     />
-                  </fieldset>
-                  <fieldset>
-                     <textarea
-                        placeholder="Which Oui'd Service you would like ?"
-                        tabindex="5"
-                        required
-                     ></textarea>
-                  </fieldset>
-                  <fieldset>
-                     <button
-                        className="btn btn-primary-success"
-                        name="submit"
-                        type="submit"
-                        id="contact-submit"
-                        data-submit="...Sending"
-                     >
-                        Submit
-                     </button>
-                  </fieldset>
-               </form>
+            <br></br>
+            <div className=" container">
+               <br></br>
+               <div className="container">
+                  <form id="contact" action="/" className="text-center">
+                     <h4>
+                        Reach out to us to schedule your Oui'd appointment!
+                     </h4>
+                     <br></br>
+                     <fieldset>
+                        <input
+                           placeholder="Full Name"
+                           type="text"
+                           tabindex="1"
+                           required
+                        />
+                     </fieldset>
+                     <fieldset>
+                        <input
+                           placeholder="Email"
+                           type="email"
+                           tabindex="1"
+                           required
+                        />
+                     </fieldset>
+                     <fieldset>
+                        <input
+                           placeholder="Best # to reach you at"
+                           type="tel"
+                           tabindex="3"
+                           required
+                        />
+                     </fieldset>
+                     <fieldset>
+                        <input
+                           placeholder="Date of appointment"
+                           type="date"
+                           tabindex="4"
+                           required
+                        />
+                     </fieldset>
+                     <fieldset>
+                        <textarea
+                           placeholder="Which Oui'd Service you would like ?"
+                           tabindex="5"
+                           required
+                        ></textarea>
+                     </fieldset>
+                     <fieldset>
+                        <button
+                           className="btn btn-primary-success"
+                           name="submit"
+                           type="submit"
+                           id="contact-submit"
+                           data-submit="...Sending"
+                        >
+                           Submit
+                        </button>
+                     </fieldset>
+                  </form>
+               </div>
             </div>
          </div>
+
+         <br></br>
       </div>
    );
 };
--- client/src/pages/Services.js
+++ client/src/pages/Services.js
@@ -1,101 +1,101 @@
 import React from "react";
 
 function Services() {
-  return (
-    // Render List of servcies
-    <div id="wrap">
-      <div id="columns" class="columns_4">
-        <figure>
-          <img src="https://assets.codepen.io/7125791/testimonal-img-3.jpg" />
-          <ul>
-            <li>Hot Stone Massage</li>
-            <li>Amazing hot stone..</li>
-          </ul>
-          <span class="price">$90</span>
-          <a class="button" href="/product">
-            Book Now
-          </a>
-        </figure>
-        <figure>
-          <img src="https://assets.codepen.io/7125791/volant-0q6QzzopVIs-unsplash.jpg" />
-          <ul>
-            <li>60 min Deep Tissue</li>
-            <li>Amazing hot stone..</li>
-          </ul>
-          <span class="price">$130</span>
-          <a class="button" href="/">
-            Book Now
-          </a>
-        </figure>
-        <figure>
-          <img src="https://assets.codepen.io/7125791/visualsofdana-IfsRTQu6Djk-unsplash.jpg" />
-          <ul>
-            <li>90 min Deep Tissue</li>
-            <li>Amazing hot stone..</li>
-          </ul>
-          <span class="price">$165</span>
-          <a class="button" href="/">
-            Book Now
-          </a>
-        </figure>
-        <figure>
-          <img src="https://assets.codepen.io/7125791/thanos-pal-lPZZXH9sjZU-unsplash.jpg" />
-          <ul>
-            <li>60 min Sweedish</li>
-            <li>Amazing hot stone..</li>
-          </ul>
-          <span class="price">$125</span>
-          <a class="button" href="/">
-            Book Now
-          </a>
-        </figure>
-        <figure>
-          <img src="https://assets.codepen.io/7125791/testimonal-img-3.jpg" />
-          <ul>
-            <li>90 Min Sweedish</li>
-            <li>Amazing hot stone..</li>
-          </ul>
-          <span class="price">$165</span>
-          <a class="button" href="/product">
-            Book Now
-          </a>
-        </figure>
-        <figure>
-          <img src="https://assets.codepen.io/7125791/volant-0q6QzzopVIs-unsplash.jpg" />
-          <ul>
-            <li>Aroma Therapy</li>
-            <li>Amazing hot stone..</li>
-          </ul>
-          <span class="price">$30</span>
-          <a class="button" href="/">
-            Book Now
-          </a>
-        </figure>
-        <figure>
-          <img src="https://assets.codepen.io/7125791/visualsofdana-IfsRTQu6Djk-unsplash.jpg" />
-          <ul>
-            <li>Herbal Wrap</li>
-            <li>Amazing hot stone..</li>
-          </ul>
-          <span class="price">$145</span>
-          <a class="button" href="/">
-            Book Now
-          </a>
-        </figure>
-        <figure>
-          <img src="https://assets.codepen.io/7125791/thanos-pal-lPZZXH9sjZU-unsplash.jpg" />
-          <ul>
-            <li>Detox Body Wrap</li>
-            <li>Amazing hot stone..</li>
-          </ul>
-          <span class="price">$60</span>
-          <a class="button" href="/">
-            Book Now
-          </a>
-        </figure>
+   return (
+      // Render List of servcies
+      <div id="wrap">
+         <div id="columns" class="columns_4">
+            <figure>
+               <img src="https://assets.codepen.io/7125791/testimonal-img-3.jpg" />
+               <ul>
+                  <li>Hot Stone Massage</li>
+                  <li>Amazing hot stone..</li>
+               </ul>
+               <span class="price">$90</span>
+               <a class="button" href="/products">
+                  Book Now
+               </a>
+            </figure>
+            <figure>
+               <img src="https://assets.codepen.io/7125791/volant-0q6QzzopVIs-unsplash.jpg" />
+               <ul>
+                  <li>60 min Deep Tissue</li>
+                  <li>Amazing hot stone..</li>
+               </ul>
+               <span class="price">$130</span>
+               <a class="button" href="/products">
+                  Book Now
+               </a>
+            </figure>
+            <figure>
+               <img src="https://assets.codepen.io/7125791/visualsofdana-IfsRTQu6Djk-unsplash.jpg" />
+               <ul>
+                  <li>90 min Deep Tissue</li>
+                  <li>Amazing hot stone..</li>
+               </ul>
+               <span class="price">$165</span>
+               <a class="button" href="/products">
+                  Book Now
+               </a>
+            </figure>
+            <figure>
+               <img src="https://assets.codepen.io/7125791/thanos-pal-lPZZXH9sjZU-unsplash.jpg" />
+               <ul>
+                  <li>60 min Sweedish</li>
+                  <li>Amazing hot stone..</li>
+               </ul>
+               <span class="price">$125</span>
+               <a class="button" href="/products">
+                  Book Now
+               </a>
+            </figure>
+            <figure>
+               <img src="https://assets.codepen.io/7125791/testimonal-img-3.jpg" />
+               <ul>
+                  <li>90 Min Sweedish</li>
+                  <li>Amazing hot stone..</li>
+               </ul>
+               <span class="price">$165</span>
+               <a class="button" href="/product">
+                  Book Now
+               </a>
+            </figure>
+            <figure>
+               <img src="https://assets.codepen.io/7125791/volant-0q6QzzopVIs-unsplash.jpg" />
+               <ul>
+                  <li>Aroma Therapy</li>
+                  <li>Amazing hot stone..</li>
+               </ul>
+               <span class="price">$30</span>
+               <a class="button" href="/products">
+                  Book Now
+               </a>
+            </figure>
+            <figure>
+               <img src="https://assets.codepen.io/7125791/visualsofdana-IfsRTQu6Djk-unsplash.jpg" />
+               <ul>
+                  <li>Herbal Wrap</li>
+                  <li>Amazing hot stone..</li>
+               </ul>
+               <span class="price">$145</span>
+               <a class="button" href="/products">
+                  Book Now
+               </a>
+            </figure>
+            <figure>
+               <img src="https://assets.codepen.io/7125791/thanos-pal-lPZZXH9sjZU-unsplash.jpg" />
+               <ul>
+                  <li>Detox Body Wrap</li>
+                  <li>Amazing hot stone..</li>
+               </ul>
+               <span class="price">$60</span>
+               <a class="button" href="/products">
+                  Book Now
+               </a>
+            </figure>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default Services;
